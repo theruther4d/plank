862e027fceb89284d1485b588fd9788ec237f283
Josh Rutherford <joshrutherford@Joshs-MacBook-Air.local>
2015-08-29 13:31:16 -0700

    use unique identifier for threads, dont publish current user to user list

--- app/client/createThreads.js
+++ app/client/createThreads.js
@@ -1,12 +1,17 @@
 Template.sidebar.events({
 	'click .username': function( e ) {
 		e.preventDefault();
+		var user1Id = Meteor.userId(),
+			user2Id = this._id,
+			jointId	= user1Id.concat( '---', user2Id );
+
+		// If there's already a thread, do nothing:
+		if( Threads.find( { _id: jointId } ).count() ) {
+			return;
+		};
 
 		var users = {
-			between: {
-				user1: Meteor.userId(),
-				user2: this._id
-			}
+			_id: jointId
 		};
 
 		Meteor.call( 'newThread', users, function(err, res) {

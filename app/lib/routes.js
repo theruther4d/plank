1ed7404d547500358dfcdc75b66bfa3b0f8f856f
Josh Rutherford <josh@Joshs-MacBook-Pro.local>
2015-09-18 09:12:11 -0700

    working on styling

--- app/lib/routes.js
+++ app/lib/routes.js
@@ -18,9 +18,9 @@ Router.route( '/', {
 		if( this.ready() ) {
 			var currUserId		= Meteor.userId(),
 				currUserThread	= Meteor.users.findOne( { _id: currUserId } ).currentThread,
-				otherUser		= Threads.findOne( { jointId: currUserThread, userId: currUserId } ).recipient;
+				otherUser		= Threads.findOne( { jointId: currUserThread, userId: currUserId } ).recipientUserName;
 
-			Router.go( '/messages/' + otherUser );
+			Router.go( '/messages/@' + otherUser );
 		}
 	}
 });
@@ -29,13 +29,6 @@ Router.route( '/messages/:_username', function(){
 	this.render( 'messages', {
 		waitOn: function() {
 			Meteor.subscribe( 'messages' );
-		},
-		data: function() {
-			var formattedUsername	= this.params._username.replace( '@', '' );
-
-			// TODO:
-			// Get the currentthread based on current user and @username, return messages
-			// from currentThread
 		}
 	});
 }, { name: 'messages' } );

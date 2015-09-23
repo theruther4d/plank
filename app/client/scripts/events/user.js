9ba8837196072ccec9d2144f70077d4210cf6072
Josh Rutherford <josh@Joshs-MacBook-Pro.local>
2015-09-22 22:35:19 -0700

    more threadhistory

--- app/client/scripts/events/user.js
+++ app/client/scripts/events/user.js
@@ -4,19 +4,22 @@ Template.user.events({
 
 		var	userId				= Meteor.userId(),
 			recipientUserId		= Meteor.users.findOne( { username: this.username } )._id,
-			existingThread		= Threads.find( { users: { $all: [ userId, recipientUserId ] } } ).count();
+			thread				= Threads.find( { users: { $all: [ userId, recipientUserId ] } } ).count();
+			hasThreadHistory	= thread ? typeof Threads.find( { users: { $all: [ userId, recipientUserId ] } } ).threadHistory !== undefined : 0;
+
 
 		// Create the thread:
 		if( !existingThread ) {
-			console.log( 'calling createThread' );
-
-			Meteor.call( 'createThread', { userId: userId, recipientUserId: recipientUserId }, function( err, result ) {
-				if( !err ) {
-					Meteor.call( 'updateThreadHistory', userId, result );
-				} else {
-					console.log( "error: ", err );
-				}
-			});
+			
+		// 	Meteor.call( 'createThread', { userId: userId, recipientUserId: recipientUserId }, function( err, result ) {
+		// 		if( !err ) {
+		// 			Meteor.call( 'updateThreadHistory', userId, result );
+		// 		} else {
+		// 			console.log( "error: ", err );
+		// 		}
+		// 	});
+		} else {
+		// 	Meteor.call( 'updateThreadHistory', userId,  );
 		}
 
 
